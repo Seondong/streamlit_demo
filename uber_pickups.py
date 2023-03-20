4a3ef82dca5d2ae98227cb781fc56ec345ca557c
Sundong Kim <sdkim0211@gmail.com>
2023-03-20 13:50:29 +0900

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -5,8 +6,10 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+@st.cache_data
 def load_data(nrows):
   data = pd.read_csv(DATA_URL, nrows=nrows)
   lowercase = lambda x: str(x).lower()
